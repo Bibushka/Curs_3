39648e89dee4d76118e43deab81c17a4e4e4bdac
Bibushka <bianca_bucur03@yahoo.com>
2017-05-10 22:25:19 +0300

    Chess test: red

---
+++ Chess/Chess/Chess.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Chess
+{
+    [TestClass]
+    public class Chess
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(5, GetNumberOfSquares(2));
+        }
+
+        public int GetNumberOfSquares(int boardLength)
+        {
+            return 0;
+        }
+    }
+}

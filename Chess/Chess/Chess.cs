bdbbe5b976ce799b2468b942ad87c99a68182516
Bibushka <bianca_bucur03@yahoo.com>
2017-05-13 09:37:00 +0300

    Chess test: green_1

--- Chess/Chess/Chess.cs
+++ Chess/Chess/Chess.cs
@@ -12,9 +12,19 @@ namespace Chess
             Assert.AreEqual(5, GetNumberOfSquares(2));
         }
 
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(204, GetNumberOfSquares(8));
+        }
+    
         public int GetNumberOfSquares(int boardLength)
         {
-            return 0;
+            int numberOfSquares = 0;
+            for (int i = 1; i <= boardLength; i++)
+                numberOfSquares = numberOfSquares + i * i;
+            return numberOfSquares;
         }
+        
     }
 }

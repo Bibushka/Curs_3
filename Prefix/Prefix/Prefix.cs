b8fb2d43e159943cc5d8e31df9af212aa6c64a03
Bibushka <bianca_bucur03@yahoo.com>
2017-05-13 12:33:00 +0300

    Prefix test: red

---
+++ Prefix/Prefix/Prefix.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Prefix
+{
+    [TestClass]
+    public class Prefix
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("yep", GetPrefix("i", "n"));
+        }
+
+        public string GetPrefix(string firstString, string secondString)
+        {
+            return "noup";
+        }
+    }
+}

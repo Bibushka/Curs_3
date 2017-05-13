1377994565d2c79b7bd345ac584c90dfea7353f7
Bibushka <bianca_bucur03@yahoo.com>
2017-05-13 12:39:33 +0300

    Prefix test: green

--- Prefix/Prefix/Prefix.cs
+++ Prefix/Prefix/Prefix.cs
@@ -9,12 +9,16 @@ namespace Prefix
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual("yep", GetPrefix("i", "n"));
+            Assert.AreEqual("aaa", GetPrefix("aaab", "aaaaam"));
         }
 
         public string GetPrefix(string firstString, string secondString)
         {
-            return "noup";
+            string prefix=string.Empty;
+            for (int i = 0; i < firstString.Length; i++)
+                if (firstString[i] == secondString[i])
+                    prefix += firstString[i];
+            return prefix;
         }
     }
 }

60f4752130edabfa0ec50d86ac1cba6b748d86dd
Bibushka <bianca_bucur03@yahoo.com>
2017-05-17 23:05:08 +0300

    Anagram test: red

---
+++ Anagram/Anagram/Anagram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Anagram
+{
+    [TestClass]
+    public class Anagram
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, GetNumberOfAnagrams("aaa"));
+        }
+
+        public int GetNumberOfAnagrams(string word)
+        {
+            return 0;
+        }
+    }
+}

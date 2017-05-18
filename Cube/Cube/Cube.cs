ed344ef8dc5e296b94ac603b0273e186f4778d9a
Bibushka <bianca_bucur03@yahoo.com>
2017-05-18 23:27:34 +0300

    Cube test: green_1

--- Cube/Cube/Cube.cs
+++ Cube/Cube/Cube.cs
@@ -7,13 +7,21 @@ namespace Cube
     public class Cube
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestForKOne()
         {
-            Assert.AreEqual(1, GetCubeNumber(1));
+            Assert.AreEqual(192, GetCubeNumber(1));
         }
 
-        public double GetCubeNumber(int k)
+        public int GetCubeNumber(int k)
         {
+            int kCount = 0;
+            for (int i = 0; i < 1000; i++)
+                if (i * i * i % 1000 == 888)
+                {
+                    kCount++;
+                    if (kCount == k)
+                        return i;
+                }
             return 0;
         }
     }

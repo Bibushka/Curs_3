75ebd0a2cabdc0c91e3008d836d9e8313e66b027
Bibushka <bianca_bucur03@yahoo.com>
2017-05-20 08:53:19 +0300

    Column test: red

---
+++ Column/Column/Column.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Column
+{
+    [TestClass]
+    public class Column
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("yep", GetColumnLetters(5));
+        }
+
+        public string GetColumnLetters(int columnIndex)
+        {
+            return "noup";
+        }
+    }
+}
